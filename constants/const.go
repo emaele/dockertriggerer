d1e5229d3f99925983957e622fa5faf6dab50dc0
Emanuele Rocco Petrone <emanueleptrn97@gmail.com>
2019-11-11 01:03:38 +0100

    update endpoint

--- constants/const.go
+++ constants/const.go
@@ -1,5 +1,6 @@
 package constants
 
-const DockerHubInfoEndpoint = "https://hub.docker.com/v2/repositories/%s/%s/tags"
+//DockerHubInfoEndpoint is the endpoint for retrieve info about a docker repository
+const DockerHubInfoEndpoint = "https://hub.docker.com/v2/repositories/%s/tags/latest"
 
-const GithubCommitEndpoint = "https://api.github.com/repos/%s/%s/commits/master"
+const GithubCommitEndpoint = "https://api.github.com/repos/%s/commits/master"
