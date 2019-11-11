6902a0f3e5724ba1282fa434920fb695a307683b
Emanuele Rocco Petrone <emanueleptrn97@gmail.com>
2019-11-11 19:08:52 +0100

    add comments

--- constants/const.go
+++ constants/const.go
@@ -3,4 +3,5 @@ package constants
 //DockerHubInfoEndpoint is the endpoint for retrieve info about a docker repository
 const DockerHubInfoEndpoint = "https://hub.docker.com/v2/repositories/%s/tags/latest"
 
+//GithubCommitEndpoint is the api endpoint for the last commit of a github repo
 const GithubCommitEndpoint = "https://api.github.com/repos/%s/commits/master"
--- types/types.go
+++ types/types.go
@@ -2,21 +2,10 @@ package types
 
 import "time"
 
+//BuildTriggerResponse contains selected fields of the POST request response
 type BuildTriggerResponse struct {
-	Autotests     string        `json:"autotests"`
-	BuildInFarm   bool          `json:"build_in_farm"`
-	BuildSettings []string      `json:"build_settings"`
-	Channel       string        `json:"channel"`
-	Deploykey     string        `json:"deploykey"`
-	Envvars       []interface{} `json:"envvars"`
-	Image         string        `json:"image"`
-	Owner         string        `json:"owner"`
-	Provider      string        `json:"provider"`
-	RepoLinks     bool          `json:"repo_links"`
-	Repository    string        `json:"repository"`
-	ResourceURI   string        `json:"resource_uri"`
-	State         string        `json:"state"`
-	UUID          string        `json:"uuid"`
+	State string `json:"state"`
+	UUID  string `json:"uuid"`
 }
 
 // GithubCommit contains selected fields of https://api.github.com/repos/org/reponame/commits/master api call
@@ -35,6 +24,7 @@ type GithubCommit struct {
 	} `json:"commit"`
 }
 
+// DockerHubRepoInfo contains selected fields of the docker info api call
 type DockerHubRepoInfo struct {
 	Name        string    `json:"name"`
 	ID          int       `json:"id"`
