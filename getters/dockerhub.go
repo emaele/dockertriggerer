d508323f56a86e1ac1569e2269f9449b53bafcc8
Emanuele Rocco Petrone <emanueleptrn97@gmail.com>
2019-11-11 01:03:20 +0100

    change docker info endpoint to get only latest tag

--- getters/dockerhub.go
+++ getters/dockerhub.go
@@ -2,7 +2,9 @@ package getters
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 
 	"gitlab.com/emaele/dockertriggerer/constants"
@@ -10,10 +12,10 @@ import (
 )
 
 //GetDockerHubRepoInfo will return repo info from hub.docker.com
-func GetDockerHubRepoInfo(org, repoName string) (types.DockerHubRepoInfo, error) {
+func GetDockerHubRepoInfo(repoName string) (types.DockerHubRepoInfo, error) {
 	var repoInfo types.DockerHubRepoInfo
 
-	resp, err := http.Get(fmt.Sprintf(constants.DockerHubInfoEndpoint, org, repoName))
+	resp, err := http.Get(fmt.Sprintf(constants.DockerHubInfoEndpoint, repoName))
 	if err != nil {
 		//TODO: Handle error
 	}
@@ -30,4 +32,33 @@ func GetDockerHubRepoInfo(org, repoName string) (types.DockerHubRepoInfo, error)
 	}
 
 	return repoInfo, nil
+}
+
+func TriggerBuild(endpoint string) error {
+	var client http.Client
+	var reader io.Reader
+
+	resp, err := client.Post(endpoint, "", reader)
+	if err != nil {
+		return err
+	}
+
+	var postResult types.BuildTriggerResponse
+
+	jdec := json.NewDecoder(resp.Body)
+	err = jdec.Decode(&postResult)
+	if err != nil {
+		return err
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	if postResult.State == "Success" {
+		return nil
+	}
+
+	return errors.New("Unable to trigger build")
 }
